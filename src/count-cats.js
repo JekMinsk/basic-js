f06d390dd1e0b0555ac3cebd39a9109f44726d37
JekMinsk <67835445+JekMinsk@users.noreply.github.com>
2020-11-01 16:39:10 +0200

    Update count-cats.js

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,11 +1,10 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(date) {
-  if(typeof(date) == 'undefined')
-    return 'Unable to determine the time of year!'
-  if(!(Object.prototype.toString.call(date) === "[object Date]") )
-    throw new Error()
-  let month = date.getMonth();
-
-  return month == 11 || month < 2 ? 'winter' : month < 5 ? 'spring' : month < 8 ? 'summer' : 'fall'
+module.exports = function countCats(matrix) {
+    let cats = 0;
+    for(const i of matrix)
+        for(const j of i)
+             if(j == '^^')
+                 cats++;  
+    return cats 
 };
